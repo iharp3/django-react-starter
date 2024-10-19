40d272904f468aa544ef1ca9b556a272098e4b89
Yuchuan Huang <itisyuchuan@gmail.com>
2024-10-19 15:00:51 -0500

    rename and format tabs

--- frontend/src/components/tabs.jsx
+++ frontend/src/components/tabs.jsx
@@ -5,77 +5,79 @@ import { Tabs as TabMui } from '@mui/material';
 import '../styles/tabs.css'
 
 function CustomTabPanel(props) {
-    const { children, value, index, ...other } = props;
-  
-    return (
-      <div
-        role="tabpanel"
-        hidden={value !== index}
-        id={`simple-tabpanel-${index}`}
-        aria-labelledby={`simple-tab-${index}`}
-        {...other}
-      >
-        {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
-      </div>
-    );
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </div>
+  );
 }
 
 CustomTabPanel.propTypes = {
-    children: PropTypes.any,
-    value: PropTypes.number,
-    index: PropTypes.number,    
+  children: PropTypes.any,
+  value: PropTypes.number,
+  index: PropTypes.number,
 };
 
 
-  function a11yProps(index) {
-    return {
-      id: `simple-tab-${index}`,
-      'aria-controls': `simple-tabpanel-${index}`,
-    };
-  }
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
 
-const Tabs = ({htmlString}) => {
+const Tabs = ({ htmlString }) => {
 
-    const [tabNum, setTab] = useState(0);
+  const [tabNum, setTab] = useState(0);
 
-    const handleChange = (event, newValue) => {        
-        setTab(newValue);
-    };    
+  const handleChange = (event, newValue) => {
+    setTab(newValue);
+  };
 
-    return (
-        <>
-            <div className="tabs_wrapper">
-                <Box sx={{ width: '100%' }}>
-                    <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                        <TabMui value={tabNum} onChange={handleChange} aria-label="Navigation">
-                            <Tab label="Rasterized Data"  />
-                            <Tab label="Times Results"  />
-                            <Tab label="Page Three" />
-                        </TabMui>
-                    </Box>
-                    <div className="page_wrapper">
-                        <CustomTabPanel value={tabNum} index={0} {...a11yProps(0)}>
-                            { !htmlString ? (
-                              <div className="">No Content</div>
-                            ) : 
-                            (                  
-                              <pre className="">{`${htmlString}`}</pre>
-                            )}
-                            {/* <div dangerouslySetInnerHTML={{ __html: htmlContent }} />                         */}
-                        </CustomTabPanel>
-                        <CustomTabPanel value={tabNum} index={1} {...a11yProps(1)}>
-                            <div className="">
-                              <h2>Times Results</h2>
-                            </div>
-                        </CustomTabPanel>
-                        <CustomTabPanel value={tabNum} index={2} {...a11yProps(2)}>
-                            Page Three
-                        </CustomTabPanel>
-                    </div>
-                </Box>
-            </div>
-       </>
-    )
+  return (
+    <>
+      <div className="tabs_wrapper">
+        <Box sx={{ width: '100%' }}>
+          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+            <TabMui value={tabNum} onChange={handleChange} aria-label="Navigation">
+              <Tab label="Raster Data" />
+              <Tab label="Time Series" />
+              <Tab label="Heatmap" />
+              <Tab label="Page Four" />
+            </TabMui>
+          </Box>
+          <div className="page_wrapper">
+            <CustomTabPanel value={tabNum} index={0} {...a11yProps(0)}>
+              {!htmlString ? (
+                <div className="">No Content</div>
+              ) :
+                (
+                  <pre className="">{`${htmlString}`}</pre>
+                )}
+              {/* <div dangerouslySetInnerHTML={{ __html: htmlContent }} />                         */}
+            </CustomTabPanel>
+            <CustomTabPanel value={tabNum} index={1} {...a11yProps(1)}>
+              Time Series
+            </CustomTabPanel>
+            <CustomTabPanel value={tabNum} index={2} {...a11yProps(2)}>
+              Heatmap
+            </CustomTabPanel>
+            <CustomTabPanel value={tabNum} index={3} {...a11yProps(3)}>
+              Page Four
+            </CustomTabPanel>
+          </div>
+        </Box>
+      </div>
+    </>
+  )
 }
 Tabs.propTypes = {
   htmlString: PropTypes.string,
