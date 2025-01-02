d06eb1d18b4ab291045ea8f2f69ebb8457fb7b66
Yuchuan Huang <itisyuchuan@gmail.com>
2025-01-02 17:52:47 -0600

    change name 2m_temperature

--- frontend/src/components/sidebar.jsx
+++ frontend/src/components/sidebar.jsx
@@ -26,7 +26,7 @@ const Sidebar = ({
                     val={variable}
                     setVal={setVariable}
                     label={"Variable"}
-                    options={["2M Temperature",
+                    options={["2m_temperature",
                         //"Surface Pressure",
                         //Total Precipitation",
                     ]}
