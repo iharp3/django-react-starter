3241c6840b5a127df3241864d9991ec06f17b71b
Yuchuan Huang <itisyuchuan@gmail.com>
2025-01-02 18:25:07 -0600

    reformat models table

--- backend/api/models.py
+++ backend/api/models.py
@@ -9,15 +9,27 @@ class Query(models.Model):
         ("month", "month"),
         ("year", "year"),
     )
+
+    SPATIAL_CHOICES = (
+        (0.25, "0.25"),
+        (0.5, "0.5"),
+        (1, "1"),
+    )
+
     AGG_CHOICES = (
         ("min", "min"),
         ("max", "max"),
         ("mean", "mean"),
     )
     REQUEST_CHOICES = (
-        ("Time Series", "Time Series"),
-        ("Heat Map", "Heat Map"),
-        ("Data Download", "Data Download"),
+        ("get_raster", "get_raster"),
+        ("timeseries", "timeseries"),
+        ("heatmap", "heatmap"),
+        ("find_time", "find_time"),
+        ("find_area", "find_area"),
+        # ("Time Series", "Time Series"),
+        # ("Heat Map", "Heat Map"),
+        # ("Data Download", "Data Download"),
     )
 
     VARIABLE_CHOICES = (
@@ -35,17 +47,17 @@ class Query(models.Model):
         ("!=", "!="),
     ]
 
-    variable = models.CharField(max_length=50, choices=VARIABLE_CHOICES, default="2M Temperature")
     requestType = models.CharField(max_length=15, choices=REQUEST_CHOICES)
-    startDateTime = models.DateTimeField(default=timezone.now)
-    endDateTime = models.DateTimeField(default=timezone.now)
-    temporalResolution = models.CharField(max_length=10, choices=TEMPORAL_CHOICES)
-    temporalAggregation = models.CharField(max_length=10, choices=AGG_CHOICES)
-    north = models.DecimalField(max_digits=40, decimal_places=35)
-    east = models.DecimalField(max_digits=40, decimal_places=35)
-    south = models.DecimalField(max_digits=40, decimal_places=35)
-    west = models.DecimalField(max_digits=40, decimal_places=35)
     created_at = models.DateTimeField(auto_now_add=True)
+    variable = models.CharField(max_length=50, choices=VARIABLE_CHOICES, default="2m_temperature")
+    startDateTime = models.DateTimeField()
+    endDateTime = models.DateTimeField()
+    temporalResolution = models.CharField(max_length=10, choices=TEMPORAL_CHOICES, default="day")
+    temporalAggregation = models.CharField(max_length=10, choices=AGG_CHOICES, default="mean")
+    north = models.DecimalField(max_digits=10, decimal_places=7)
+    east = models.DecimalField(max_digits=10, decimal_places=7)
+    south = models.DecimalField(max_digits=10, decimal_places=7)
+    west = models.DecimalField(max_digits=10, decimal_places=7)
     secondAgg = models.CharField(max_length=25, choices=AGG_CHOICES, default="min")
     filterPredicate = models.CharField(max_length=2, choices=FILTER_PREDICATE_CHOICES, default="=")
     filterValue = models.FloatField(default=255.0)
