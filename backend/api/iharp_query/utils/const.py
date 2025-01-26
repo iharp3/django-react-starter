0473b03904d4b4525384306bcdf4082b45a727fd
Yuchuan Huang <itisyuchuan@gmail.com>
2025-01-26 16:32:20 -0600

    remove deprecated query code

--- backend/api/iharp_query/utils/const.py
+++
@@ -1,8 +0,0 @@
-RAW_DATA_PATH = "/data/era5/raw"
-AGG_DATA_PATH = "/data/era5/agg"
-
-long_short_name_dict = {
-    "2m_temperature": "t2m",
-    "surface_pressure": "sp",
-    "sea_surface_temperature": "sst",
-}
