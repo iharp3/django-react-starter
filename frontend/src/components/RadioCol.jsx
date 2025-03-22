41b5f74a46250eb106eeafb8678bb14043a5ec9b
ana-e-uk <uribe055@umn.edu>
2025-03-21 21:57:46 -0500

    add border, control titles

--- frontend/src/components/RadioCol.jsx
+++ frontend/src/components/RadioCol.jsx
@@ -4,26 +4,31 @@ import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
+import { Grid2 } from "@mui/material";
 
 const RadioButtonsCol = ({ label, options, val, setVal, subLabel, defaultValue }) => {
-
     return (
-        <FormControl sx={{}}>
+        <FormControl sx={{ width: "48%" }}>
             <FormLabel id={label}>{label}</FormLabel>
             <RadioGroup
                 defaultValue={defaultValue}
-                column
                 aria-labelledby={label}
                 name={subLabel}
-                sx={{ width: "45%", height:"25%", scale: "95%", justifyContent: "center", border: "" }}
                 value={val}
                 onChange={setVal}
             >
-                {options.map((option) => {
-                    return (
-                        <FormControlLabel key={option} value={option} control={<Radio />} label={option} />
-                    );
-                })}
+                <Grid2 container spacing={0.5}>
+                    {options.map((option, index) => (
+                        <Grid2 item xs={6} key={option} sx={{ padding: "0px" }}>
+                            <FormControlLabel 
+                                value={option} 
+                                control={<Radio size="x-small" />} 
+                                label={option} 
+                                sx={{ margin: 0 }}
+                            />
+                        </Grid2>
+                    ))}
+                </Grid2>
             </RadioGroup>
         </FormControl>
     );
--- frontend/src/components/sidebar.jsx
+++ frontend/src/components/sidebar.jsx
@@ -7,9 +7,9 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import dayjs from 'dayjs';
 import PropTypes from 'prop-types';
-import { Button, TextField } from '@mui/material'
-import '../styles/sidebar.css'
-import '../styles/loading.css'
+import { Button, TextField } from '@mui/material';
+import '../styles/sidebar.css';
+import '../styles/loading.css';
 
 const Sidebar = ({
     setComparisonVal, 
@@ -23,8 +23,8 @@ const Sidebar = ({
     formData,
     handleChange,
     queryData,
-    isLoading, }) => {
-
+    isLoading,
+}) => {
     return (
         <>
             <div className="sidebar_wrapper">
@@ -36,124 +36,96 @@ const Sidebar = ({
                     val={variable}
                     setVal={setVariable}
                     label={"Variable"}
-                    options={[
-                        "2m_temperature",
-                        "total_precipitation",
-                        "surface_pressure",
-                        "snow_depth",
-                        "snowfall",
-                        "snowmelt",
-                        "temperature_of_snow_layer",
-                        "ice_temperature_layer_1",
-                        "ice_temperature_layer_2",
-                        "ice_temperature_layer_3",
-                        "ice_temperature_layer_4",
-                    ]}
+                    options={["2m_temperature", "total_precipitation", "surface_pressure", "snow_depth", "snowfall", "snowmelt", "temperature_of_snow_layer", "ice_temperature_layer_1", "ice_temperature_layer_2", "ice_temperature_layer_3", "ice_temperature_layer_4"]}
                     sx={{ width: "95%"}}
                     size={"small"}
                     varLabel={"variable"}
                 />
-                <CardinalDirections formData={formData} handleChange={handleChange} />
-                <div className="padding"/>
-                <RadioButtonsRow
-                    label="Spatial Resolution"
-                    options={[0.25, 0.5, 1]}
-                    var={formData.spatialResolution}
-                    setVal={handleChange}
-                    subLabel="spatialResolution"
-                    defaultValue={1} 
-                />
-                <div className="padding"/>
-                <div className="row_wrapper">
+                
+                <div className="control_section">
+                    <p className="section_title">Spatial Controls</p>
+                    <CardinalDirections formData={formData} handleChange={handleChange} />
+                    <div className="padding"/>
+                    <RadioButtonsRow 
+                        label="Spatial Resolution" 
+                        options={[0.25, 0.5, 1]} 
+                        var={formData.spatialResolution} 
+                        setVal={handleChange} 
+                        subLabel="spatialResolution" 
+                        defaultValue={1} />
+                </div>
+
+                <div className="control_section">
+                    <p className="section_title">Temporal Controls</p>
+                    <div className="row_wrapper">
                     <LocalizationProvider dateAdapter={AdapterDayjs}>
                         <div className="half_column_wrapper">
-                            <DateTimePicker
-                                label="Start Date Time"
-                                views={['year', 'day', 'hours']}
-                                ampm={false}
-                                value={startDate ? dayjs(startDate) : null}
-                                onChange={(newValue) => setStartDate(newValue ? newValue.format('YYYY-MM-DD HH:mm') : null)}
-                            />
-                            <DateTimePicker
-                                label="End Date Time"
-                                views={['year', 'day', 'hours']}
-                                ampm={false}
-                                value={endDate ? dayjs(endDate) : null}
-                                onChange={(newValue) => setEndDate(newValue ? newValue.format('YYYY-MM-DD HH:mm') : null)}
-                            />
+                            <DateTimePicker 
+                                label="Start Date Time" 
+                                views={['year', 'day', 'hours']} 
+                                ampm={false} 
+                                value={startDate ? dayjs(startDate) : null} 
+                                onChange={(newValue) => setStartDate(newValue ? newValue.format('YYYY-MM-DD HH:mm') : null)} />
+                            <DateTimePicker 
+                                label="End Date Time" 
+                                views={['year', 'day', 'hours']} 
+                                ampm={false} 
+                                value={endDate ? dayjs(endDate) : null} 
+                                onChange={(newValue) => setEndDate(newValue ? newValue.format('YYYY-MM-DD HH:mm') : null)} />
                         </div>
                     </LocalizationProvider>
-                    <RadioButtonsCol
-                        label="Temporal Resolution"
-                        options={["hour", "day", "month", "year"]}
-                        var={formData.temporalResolution}
-                        setVal={handleChange}
-                        subLabel="temporalResolution"
+                    <RadioButtonsCol 
+                        label="Temporal Resolution" 
+                        options={["hour", "day", "month", "year"]} 
+                        var={formData.temporalResolution} setVal={handleChange} 
+                        subLabel="temporalResolution" 
                         defaultValue={"year"} />
-                </div>
-                <RadioButtonsRow
-                    label="Spatio-Temporal Aggregation"
-                    options={["min", "max", "mean"]}
-                    var={formData.aggregation}
-                    setVal={handleChange}
-                    subLabel="aggregation"
-                    defaultValue={"mean"} 
-                />
-                <div className="padding" />
-                <div className="pred_value">
-                    <Input
-                        name="predicate"
-                        label={"Predicate"}
-                        options={["<", ">", "<=", ">=", "!="]}
-                        sx={{ width: "25%" }}
-                        size={"small"}
-                        val={formData.filterPredicate}
-                        setVal={setPredicate} 
-                    />
-                    <TextField
-                        id="outlined-number"
-                        label="Value"
-                        type="number"
-                        sx={{ width:"25%" }}
-                        slotProps={{
-                            inputLabel: {
-                            shrink: true,
-                            },
-                        }}
-                        value={formData.filterValue}
-                        onChange={(e) => { setComparisonVal(e.target.value) }} 
-                    />
-                </div>
-                <div className="padding"/>
-                <Button
-                    onClick={() => queryData()}
-                    variant="outlined"
-                    disabled={isLoading}
-                    sx={{ marginBottom: "48px", marginTop: "auto" }}
-                >
-                    <div className="button-content">
-                        {isLoading && <div className="loading-spinner" />}
-                        Query
                     </div>
+                </div>
+
+                <div className="control_section">
+                    <p className="section_title">Aggregation</p>
+                    <RadioButtonsRow 
+                        label="Spatio-Temporal Aggregation" 
+                        options={["min", "max", "mean"]} 
+                        var={formData.aggregation} 
+                        setVal={handleChange} 
+                        subLabel="aggregation" 
+                        defaultValue={"mean"} />
+                </div>
+
+                <div className="control_section">
+                    <p className="section_title">Find Controls</p>
+                    <div className="pred_value">
+                        <Input 
+                            name="predicate" 
+                            label={"Predicate"} 
+                            options={["<", ">", "<=", ">=", "!="]} 
+                            sx={{ width: "25%" }} 
+                            size={"small"} 
+                            val={formData.filterPredicate} 
+                            setVal={setPredicate} />
+                        <TextField 
+                            id="outlined-number" 
+                            label="Value" 
+                            type="number" 
+                            sx={{ width:"25%" }} 
+                            value={formData.filterValue} 
+                            onChange={(e) => { setComparisonVal(e.target.value) }} />
+                    </div>
+                </div>
+                
+                <div className="padding"/>
+                <Button 
+                    onClick={() => queryData()} 
+                    variant="outlined" 
+                    disabled={isLoading} 
+                    className="query_button">
+                <div className="button-content">{isLoading && <div className="loading-spinner" />} Query</div>
                 </Button>
             </div>
         </>
     )
 }
-
-Sidebar.propTypes = {
-    setComparisonVal: PropTypes.func, 
-    setPredicate: PropTypes.func,
-    variable: PropTypes.any,
-    setVariable: PropTypes.func,
-    startDate: PropTypes.any,
-    endDate: PropTypes.any,
-    setStartDate: PropTypes.func,
-    setEndDate: PropTypes.func,
-    formData: PropTypes.object,
-    handleChange: PropTypes.func,
-    queryData: PropTypes.func,
-    isLoading: PropTypes.bool,
-};
 
 export default Sidebar;
