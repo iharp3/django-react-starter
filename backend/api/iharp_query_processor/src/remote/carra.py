5b719d9db2a1f168321c54ec5835aea2795e0be6
ana-e-uk <uribe055@umn.edu>
2026-03-12 11:40:02 -0500

    directory typo

--- backend/api/iharp_query_processor/src/remote/carra.py
+++ backend/api/iharp_query_processor/src/remote/carra.py
@@ -12,7 +12,7 @@ class CARRARepository(RemoteRepository):
 
         ts = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
 
-        return f"/data/cara/carra_{ts}.nc"
+        return f"/data/carra/carra_{ts}.nc"
     
     # TODO: write function to turn lat/lon info into East or West domain
     
